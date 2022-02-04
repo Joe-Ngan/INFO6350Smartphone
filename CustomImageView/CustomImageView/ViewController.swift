3d6962933eafe3a5594d037519624ea64a0333a4
Joe-Ngan <yan.jinz@northeastern.edu>
2022-02-03 16:59:39 -0800

    Checking in code for adding pics and captions

--- CustomImageView/CustomImageView/ViewController.swift
+++ CustomImageView/CustomImageView/ViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
-    let arr = ["food1","food2","food3","food4","food5","food6","food7","food8","food9"]
-    let lable = ["food1","food2","food3","food4","food5","food6","food7","food8","food9"]
+    let images = ["food1","food2","food3","food4","food5","food6","food7","food8","food9"]
+    let imageNames = ["food1","food2","food3","food4","food5","food6","food7","food8","food9"]
 
     @IBOutlet weak var tblView: UITableView!
     override func viewDidLoad() {
@@ -18,11 +18,13 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
         // Do any additional setup after loading the view.
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arr.count
+        return images.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as!TableViewCell
+        cell.imgView.image = UIImage(named: images[indexPath.row])
+        cell.lblCell.text = imageNames[indexPath.row]
         return cell
     }
 
